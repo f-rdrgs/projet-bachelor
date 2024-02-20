e3fe2be324b726266a9d74f2f91282129a6b28a3
fabio.rodrigue1 <fabio.rodrigues-dos-santos@etu.hesge.ch>
2024-02-21 00:48:34 +0100

    Ajout détection de quelques resources fixes comme tennis, pétanque et badminton

--- proto/CHATBOT/main.py
+++ proto/CHATBOT/main.py
@@ -1,2 +1,42 @@
+import requests
+import os
+
+# Training example from https://towardsdatascience.com/building-a-conversational-chatbot-for-slack-using-rasa-and-python-part-1-bca5cc75d32f#bypass 
+# from rasa_nlu.training_data import load_data
+# from rasa_nlu.config import RasaNLUModelConfig
+# from rasa_nlu.model import Trainer
+# from rasa_nlu import config
+
+# # loading the nlu training samples
+# training_data = load_data("nlu.md")
+
+# # trainer to educate our pipeline
+# trainer = Trainer(config.load("config.yml"))
+
+# # train the model!
+# interpreter = trainer.train(training_data)
+
+# # store it for future use
+# model_directory = trainer.persist("./models/nlu", fixed_model_name="current")
+
+
+def send_message_to_rasa(message):
+    url = 'http://localhost:5005/webhooks/rest/webhook' # Assuming Rasa server is running locally on port 5005
+    data = {'sender': 'user','message': message}
+    response = requests.post(url, json=data,headers={
+        'Content-Type': 'application/json'
+    })
+    return response.json()
+
+# https://rasa.com/docs/rasa/training-data-format/#lookup-tables
+# https://rasa.com/docs/rasa/nlu-training-data/#entities-roles-and-groups
+# D'abord lancer le serveur
+# rasa run --enable-api --port 5005  --cors '*' -m models/my_rasa_model.tar.gz
+# Ensuite lancer le serveur d'actions custom
+# rasa run actions
+# Pour train
+# rasa train --fixed-model-name my_rasa_model
 if __name__ == "__main__":
-    _=_
+    user_input = input("Your input -> ")
+    response = send_message_to_rasa(user_input)
+    print("Rasa's response ->", response)
---
+++ proto/CHATBOT/test_reservation/actions/actions.py
@@ -0,0 +1,29 @@
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
+
+
+# This is a simple example for a custom action which utters "Hello World!"
+
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+
+class ActionReserveResource(Action):
+
+    def name(self) -> Text:
+        return "action_reserve_resource"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        current_resource = next(tracker.get_latest_entity_values("resource"),None)
+        print(current_resource)
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
