1d09eeb19ee71de3994d13b648055e2c2fa28235
fabio.rodrigue1 <fabio.rodrigues-dos-santos@etu.hesge.ch>
2024-05-15 14:49:40 +0200

    Ajout docker API et Chatbot (pas tout fonctionnel)

---
+++ projet/api/README.md
@@ -0,0 +1,11 @@
+# API Rasa chatbot
+
+## Prérequis
+
+## Utilisation
+
+Pour lancer le serveur
+>docker compose -f api-docker-compose.yml up
+
+Pour recompiler en cas de changement de `server.py`
+>docker compose -f api-docker-compose.yml up --build --force-recreate api
---
+++ projet/api/server.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, status
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+import uvicorn
+import asyncio
+import httpx
+
+app = FastAPI()
+
+class Communicate_Rasa(BaseModel):
+    sender:str
+    message:str
+
+origins = [
+    "http://0.0.0.0",
+    "http://0.0.0.0:5500",
+    "http://localhost:5500",
+    "http://localhost",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/communicate-rasa")
+async def talk_to_rasa(data:Communicate_Rasa):
+    headers = {"Content-Type": "application/json"}
+    print(data.model_dump_json())
+    async with httpx.AsyncClient() as client:
+        response = await client.post('http://localhost:5005/webhooks/rest/webhook',json={"sender":data.sender,"message":data.message},headers=headers)    
+        return JSONResponse(content=response.json(),status_code=status.HTTP_200_OK)
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0" , port=5500, log_level="info",reload = True,workers=4)
