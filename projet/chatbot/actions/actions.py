cb3339d5e24f34fe2489e734a97d61770824d2c2
fabio.rodrigue1 <fabio.rodrigues-dos-santos@etu.hesge.ch>
2024-05-21 22:29:00 +0200

    Réservation in-memory très simple d'une ressource

--- projet/chatbot/actions/actions.py
+++ projet/chatbot/actions/actions.py
@@ -9,8 +9,14 @@
 
 from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
+from rasa_sdk import Action, Tracker, FormValidationAction, ValidationAction
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.types import DomainDict
+from rasa_sdk.events import EventType
+
+@staticmethod
+def get_ressource_list()->list[str]:
+    return ["tennis","badminton","ping-pong","pétanque","handball","basketball"]
 
 class ActionCheckRessource(Action):
 
@@ -23,4 +29,74 @@ class ActionCheckRessource(Action):
 
         dispatcher.utter_message(text="Hello World!")
 
+        return []
+
+class ActionEnumerateRessource(Action):
+
+    def name(self) -> Text:
+        return "action_enumerate_ressource"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Les ressources à disposition sont : \n\t- Tennis\n\t- Pétanque\n\t- Handball\n\t- basketball\n\t- ping-pong")
+
+        return []
+    
+# https://learning.rasa.com/rasa-forms-3/validation/
+class ValidateRessourceForm(FormValidationAction):
+    def name(self)->Text:
+        return "validate_get_ressource_form"
+    def validate_ressource(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        ressource = str(slot_value).lower()
+        print(f"RESSOURCE : {ressource}")
+        if ressource in get_ressource_list():
+            return {"ressource":ressource}
+        dispatcher.utter_message(text=f"{ressource} n'existe pas. Veuillez réserver une ressource qui existe")
+        print("LEAVING FORM")
+        return {"ressource":None}
+
+# https://rasa.com/docs/rasa/action-server/validation-action/
+class ValidateRessourceSlot(ValidationAction):
+    def validate_ressource(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        """Validate location value."""
+        if isinstance(slot_value, str):
+            ressource :str = str(slot_value).lower()
+            dispatcher.utter_message(f"Ressource: {ressource}")
+
+            if ressource in get_ressource_list():
+                return {"ressource": ressource.capitalize()}
+            else:
+                dispatcher.utter_message(f"{ressource.capitalize()} n'existe pas. Veuillez suggérer une ressource valide.")
+                return {"ressource": None}
+        else:
+            # validation failed, set this slot to None
+            return {"ressource": None}
+        
+# https://rasa.com/docs/rasa/forms/#using-a-custom-action-to-ask-for-the-next-slot
+class AskForRessourceAction(Action):
+    def name(self) -> Text:
+        return "action_ask_ressource"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        response_mess = "Les ressources possible à réserver sont :"
+        for ress in get_ressource_list():
+            response_mess += f"\n\t- {ress}"
+        dispatcher.utter_message(text=response_mess)
+        dispatcher.utter_message(text="Que souhaitez-vous réserver ?")
         return []
