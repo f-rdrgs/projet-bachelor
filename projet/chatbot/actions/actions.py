04bfb302949dea60bf4a945fc44ea3be6f87eb2d
fabio.rodrigue1 <fabio.rodrigues-dos-santos@etu.hesge.ch>
2024-05-24 18:15:32 +0200

    Bot remarche pour réservation temps et heure, implémenter validation heure temps

--- projet/chatbot/actions/actions.py
+++ projet/chatbot/actions/actions.py
@@ -7,17 +7,47 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+from enum import Enum
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker, FormValidationAction, ValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
-from rasa_sdk.events import EventType
+from rasa_sdk.events import EventType, UserUtteranceReverted, FollowupAction
 from rasa_sdk.events import SlotSet
+import datetime
+
+
+class Day_week(Enum):
+    lundi = 0
+    mardi = 1
+    mercredi = 2
+    jeudi = 3
+    vendredi = 4
+    samedi = 5
+    dimanche = 6    
+
+
 @staticmethod
 def get_ressource_list()->list[str]:
     return ["tennis","badminton","ping-pong","pétanque","handball","basket"]
 
+@staticmethod
+def get_jours_semaine()->list:
+    return [Day_week.lundi.value,Day_week.mercredi.value,Day_week.jeudi.value,Day_week.vendredi.value]
+
+@staticmethod
+def get_heures():
+    return ["11h00","11h30","12h00","13h00","13h30","14h00"]
+
+@staticmethod
+def get_dates()->list[datetime.date]:
+    curr_date = datetime.datetime.now().date()
+    dates_dispo = []
+    for date in range(30):
+        if (curr_date + datetime.timedelta(days=date)).weekday() in get_jours_semaine():
+            dates_dispo.append(curr_date + datetime.timedelta(days=date))
+    return dates_dispo
 class ActionCheckRessource(Action):
 
     def name(self) -> Text:
@@ -31,25 +61,57 @@ class ActionCheckRessource(Action):
         if ressource not in get_ressource_list():
             dispatcher.utter_message(text=f"{ressource.capitalize()} n'existe pas. Veuillez réessayer avec une ressource valide.")
             SlotSet("ressource",None)
-            return []
+            return [FollowupAction()]
         
         SlotSet("ressource",ressource)
         dispatcher.utter_message(tracker.get_slot("ressource"))
         return []
 
-class ActionEnumerateRessource(Action):
+class ActionSalutation(Action):
 
     def name(self) -> Text:
-        return "action_enumerate_ressource"
+        return "action_salutation"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        response_mess = "Les ressources possible à réserver sont :"
+        for ress in get_ressource_list():
+            response_mess += f"\n\t- {ress}"
+        dispatcher.utter_message(text="Bonjour!")
+        dispatcher.utter_message(text=response_mess)
+        dispatcher.utter_message(text="Que souhaitez-vous réserver ?")
+       
+        return []
+
+
+class ActionConfirmSwitchRessource(Action):
+
+    def name(self) -> Text:
+        return "action_ask_switch_ressource"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(text="Les ressources à disposition sont : \n\t- Tennis\n\t- Pétanque\n\t- Handball\n\t- basket\n\t- ping-pong")
+        dispatcher.utter_message(text=f"Voulez-vous vraiment réserver {next(tracker.get_latest_entity_values('ressource'),None).lower()} à la place ?")
 
         return []
     
+class ActionSwitchRessource(Action):
+
+    def name(self) -> Text:
+        return "action_switch_ressource"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        SlotSet("ressource",next(tracker.get_latest_entity_values("ressource"),None).lower())
+
+        return []
+    
+
 # https://learning.rasa.com/rasa-forms-3/validation/
 class ValidateRessourceForm(FormValidationAction):
     def name(self)->Text:
@@ -68,6 +130,75 @@ class ValidateRessourceForm(FormValidationAction):
         dispatcher.utter_message(text=f"{ressource} n'existe pas. Veuillez réserver une ressource qui existe")
         print("LEAVING FORM")
         return {"ressource":None}
+
+class ValidateHeuresForm(FormValidationAction):
+    def name(self)->Text:
+        return "validate_get_date_heure_form"
+    # Duckling ne gère pas les dates sous forme de 15.05 ! ! !
+    def validate_date(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        ressource = str(slot_value)
+        jours_dispo = get_jours_semaine()
+
+        dispatcher.utter_message(text=f"Date : {ressource}")
+
+      
+        return {"date":ressource}
+
+    def validate_heure(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        ressource = str(slot_value)
+        jours_dispo = get_jours_semaine()
+
+        dispatcher.utter_message(text=f"Heure : {ressource}")
+
+      
+        return {"heure":ressource}
+    
+    # async def extract_date(self,
+    #     dispatcher: CollectingDispatcher,
+    #     tracker: Tracker,
+    #     domain: DomainDict,
+    # ) -> Dict[Text, Any]:
+    #     last_intent = tracker.latest_message.get("intent")["name"]
+    #     dispatcher.utter_message(tracker.latest_message)
+    #     dispatcher.utter_message(f"INTENT :{last_intent}")
+    #     if last_intent == "inform_date":
+    #         date_value = next(tracker.get_latest_entity_values("date"),None)
+    #         dispatcher.utter_message(f"VALUE DATE: {date_value}")
+    #         # SlotSet("date",date_value)
+    #         return {"date":date_value}
+
+    #     return []
+    
+    # async def extract_heure(self,
+    #     dispatcher: CollectingDispatcher,
+    #     tracker: Tracker,
+    #     domain: DomainDict,
+    # ) -> Dict[Text, Any]:
+    #     last_intent = tracker.latest_message.get("intent")["name"]
+    #     dispatcher.utter_message(tracker.latest_message)
+    #     dispatcher.utter_message(f"INTENT :{last_intent}")
+    #     if last_intent == "inform_heure":
+    #         heure_value = next(tracker.get_latest_entity_values("heure"),None)
+    #         dispatcher.utter_message(f"VALUE heure: {heure_value}")
+    #         # SlotSet("heure",heure_value)
+    #         return {"heure":heure_value}
+
+    #     return []
+
+
+
 
 # https://rasa.com/docs/rasa/action-server/validation-action/
 # class ValidateRessourceSlot(ValidationAction):
@@ -105,4 +236,32 @@ class AskForRessourceAction(Action):
             response_mess += f"\n\t- {ress}"
         dispatcher.utter_message(text=response_mess)
         dispatcher.utter_message(text="Que souhaitez-vous réserver ?")
+        return []
+    
+class AskForHeureAction(Action):
+    def name(self) -> Text:
+        return "action_ask_heure"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        response_mess = "Les heures disponibles à la réservation sont :"
+        for ress in get_heures():
+            response_mess += f"\n\t- {ress}"
+        dispatcher.utter_message(text=response_mess)
+        dispatcher.utter_message(text="Pour quelle heure souhaitez-vous réserver ?")
+        return []
+    
+class AskForDateAction(Action):
+    def name(self) -> Text:
+        return "action_ask_date"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        response_mess = "Les dates disponibles à la réservation sont :"
+        for date  in get_dates():
+            response_mess += f"\n\t- {date.day}/{date.month}/{date.year}"
+        dispatcher.utter_message(text=response_mess)
+        dispatcher.utter_message(text="Quand souhaitez-vous réserver ?")
         return []
