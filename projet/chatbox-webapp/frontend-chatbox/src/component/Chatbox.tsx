ede7b55e960aad0fbd11bf9a0f648352557bde8d
fabio.rodrigue1 <fabio.rodrigues-dos-santos@etu.hesge.ch>
2024-07-14 22:15:04 +0200

    ajout radio bouton choix frontend

--- projet/chatbox-webapp/frontend-chatbox/src/component/Chatbox.tsx
+++ projet/chatbox-webapp/frontend-chatbox/src/component/Chatbox.tsx
@@ -29,8 +29,11 @@ interface ChatboxMessage {
 }
 
 interface ChatboxRadio extends ChatboxMessage {
-    radio:Component_Radio,
-    type:'radio'
+    titles: string[],
+    elements_id_array: string[][],
+    texts_array:string[][],
+    type:'radio',
+    addMessage: (message:string, index?:number) =>void
 }
 
 
@@ -41,7 +44,8 @@ interface ChatboxText extends ChatboxMessage{
 }
 
 interface ChatboxMessageCont{
-    chatList:ChatboxMessage[]
+    chatList:ChatboxMessage[],
+    addMessage: (message:string, index?:number) =>void
 }
 // https://felixgerschau.com/react-typescript-components/
 
@@ -66,18 +70,56 @@ const Chatbox_text:FC<ChatboxText> = ({message,isBot,key_value}) => {
 };
 
 
-const Radio_container : FC<ChatboxRadio> = ({radio,key_value}) => {
-    const radio_elements = radio.titles.map((title,index) => {
-        return <Radio_element elements_id={radio.elements_id_array[index]} title={title} texts={radio.texts_array[index]}/>
+const Radio_container : FC<ChatboxRadio> = ({titles,elements_id_array,texts_array,key_value,addMessage}) => {
+    
+    const [radioChoices,setRadioChoices] = useState<{[name:string]:string}>({})
+    const [radioIndex,setRadioIndex] = useState<number>(0)
+    function addtoChoices(e:React.MouseEvent<HTMLInputElement>){
+        const value = (e.target as HTMLInputElement).value;
+        const target_name = (e.target as HTMLInputElement).name;
+        setRadioChoices((prevChoices) => {
+            return {...prevChoices,[target_name]:value}
+        })
+    }
+
+    function submitRadio() {
+        console.log("CHOICES: "+Object.keys(radioChoices).length)
+        const lenChoices = Object.keys(radioChoices).length
+        if(lenChoices> 0)
+        {
+            let newMessage :string = ""
+            for(let key in radioChoices){
+                newMessage += radioChoices[key]+" "
+
+            }
+            console.log(newMessage)
+            setRadioIndex((prevIndex) => {
+                const len = prevIndex+ lenChoices
+                return len
+            })
+            setRadioChoices({})
+            addMessage(newMessage.trim())
+            
+        }
+    }
+
+    useEffect(() => {
+        console.log("RADIO CHOICES: ", radioChoices);
+        console.log("RADIO INDEX: "+radioIndex)
+    }, [radioChoices,radioIndex]);
+
+    const radio_elements = titles.map((title,index) => {
+        return <Radio_element addToChoices={addtoChoices} radio_id={key_value + index} key={index+key_value} elements_id={elements_id_array[index]} title={title} texts={texts_array[index]}/>
     })
     return (
         <>
             {radio_elements}
+            <button onClick={submitRadio} className="radio-submit" type="submit" name="send-radio">Confirmer</button>
         </>
     )
 }
 
-const Chatbox_message_container: FC<ChatboxMessageCont> = ({chatList}) => {
+const Chatbox_message_container: FC<ChatboxMessageCont> = ({chatList,addMessage}) => {
 
     const bottomChatRef = useRef<HTMLDivElement>(null);
 
@@ -96,7 +138,7 @@ const Chatbox_message_container: FC<ChatboxMessageCont> = ({chatList}) => {
                 return <Chatbox_text key={index} key_value={index} isBot={message_text.isBot} message={message_text.message} type="text"/>
             } else if (message.type === 'radio'){
                 const message_radio :ChatboxRadio = (message as ChatboxRadio);
-                return <Radio_container radio={message_radio.radio} type="radio" key_value={message.key_value} key={index}/>
+                return <Radio_container addMessage={addMessage} elements_id_array={message_radio.elements_id_array} texts_array={message_radio.texts_array} titles={message_radio.titles}  type="radio" key_value={message.key_value} key={index}/>
             }else{
                 return <div/>
             }
@@ -132,10 +174,13 @@ const Chatbox_container: FC<ChatboxContainer> = ({}) => {
 
     const [messages,setMessages] = useState<ChatboxMessage[]>([])
 
+    const [radioIndex,setRadioIndex] = useState(0)
+
     const [user_id] = useState(uuidv4())
 
+
     // \[(.*?)\]
-    // [OPTION][0][Avec ou sans couverts ?][(Avec,1)(Sans,2)(Je sais pas,3)][OPTION][1][Avec ou sans chaussures ?][(Avec,1)(Sans,2)(Je sais pas,3)]
+    // [OPTION][0][TITLE][Avec ou sans couverts ?][OPTIONS][(Avec,1)(Sans,2)(Je sais pas,3)][OPTION][1][TITLE][Avec ou sans chaussures ?][OPTIONS][(Avec,1)(Sans,2)(Je sais pas,3)]
     function process_message(newMessage:string, index ?: number) : ChatboxMessage{
 
         const parse_array = (text:string) =>  {
@@ -148,8 +193,72 @@ const Chatbox_container: FC<ChatboxContainer> = ({}) => {
             const options = newMessage.split('[OPTION]').filter((message,index) => message.length>0)
             // Parser le message radio bouton et créer les éléments nécessaires, possiblement changer le retour de fonction en array de ChatboxMessages vu qu'il peut y avoir une multitude de radio boutons
             // const ids = options[]
-            options.forEach(console.log)
-            const radio_message : ChatboxRadio = {radio:{elements_id_array:[], texts_array:[],titles:[]},key_value:(index != undefined ? index : 0),type:'radio'}
+
+            let titles : string[] = [];
+            let components_id : string[][] = [];
+            let texts_array: string[][] = [];
+            
+            const regex_title = /\[TITLE\]\[(.*?)\]/;
+            const regex_index = /^\[(.*?)\]/;
+            const regex_options = /\[OPTIONS]\[(.*?)\]/;
+            const regex_single_opt = /\((.*?)\)/g;
+
+            // const regex_single_opt_elems = /(.*?),(.*?)/g;
+            options.forEach((option,index) => {
+                let radio_index = 0;
+                // console.log("OPTION: "+option)
+                const match_title = option.match(regex_title);
+                const match_index = option.match(regex_index);
+                const match_options = option.match(regex_options);
+                if(match_index !== null)
+                    {
+                        radio_index = +match_index[1]
+                        // console.log("INDEX: "+radio_index)
+                        components_id[radio_index] = []
+                        texts_array[radio_index] = []
+                        if(match_title !== null)
+                        {
+                            // console.log(match_title[1]);
+                            titles.push(match_title[1]);
+                        }
+                            
+                    }
+                    
+                if(match_options !== null)
+                {
+
+                    // texts_array[radioIndex].push()
+                    const opts = match_options[1]
+                    // console.log("OPTIONS: " +match_options[1])
+                    let matched_opts = opts.match(regex_single_opt);
+                    
+                    if (matched_opts !== null)
+                    {
+                        const new_matched_opts = matched_opts.map((elem,_) => { return elem.replace('(',"").replace(')',"")}).map((elem,_) => elem.split(','))
+                        // console.log(new_matched_opts)
+                        new_matched_opts.forEach((elems,_) => {
+                            // console.log("ELEMS: "+elems)
+                            components_id[radio_index] = [...components_id[radio_index],elems[1]]
+                            texts_array[radio_index] = [...texts_array[radio_index],elems[0]]
+                        })
+                    }
+                }
+
+                
+            })
+            // console.log(options.length)
+            setRadioIndex((prevIndex)=> prevIndex+options.length);
+
+            // console.log(titles)
+            // console.log(components_id)
+            // console.log(texts_array)
+
+            
+
+                
+            
+            
+            const radio_message : ChatboxRadio = {elements_id_array:components_id, texts_array:texts_array,titles:titles,key_value:radioIndex,type:'radio',addMessage}
             return radio_message;
         } else{
             const text_message : ChatboxText= {message:newMessage,isBot:true,key_value:(index != undefined ? index : 0),type:'text'}
@@ -168,7 +277,7 @@ const Chatbox_container: FC<ChatboxContainer> = ({}) => {
     }
 
     useEffect(()=>{
-
+        console.log("Radio index: "+radioIndex)
         socket.on('chat', (bot_messages:string[])=> {
             const messagesBotTreated : ChatboxMessage[]= bot_messages.map((message,index)=>{
                 return  process_message(message,index);
@@ -181,7 +290,7 @@ const Chatbox_container: FC<ChatboxContainer> = ({}) => {
         return () => {
             socket.off('chat');
         };
-    },[])
+    },[radioIndex])
 
     function sendMessageToBot(message:string,uuid:string){
         socket.emit("chat", message,uuid)
@@ -198,7 +307,7 @@ const Chatbox_container: FC<ChatboxContainer> = ({}) => {
 						{/* <!-- Here --> */}
                         <div className="container">
             <Chatbox_header header="Réservation"/>
-            <Chatbox_message_container chatList={messages}/>
+            <Chatbox_message_container addMessage={addMessage} chatList={messages}/>
             <Chatbox_textbox_cont addMessage={addMessage}/>
         </div>
 					{/* <!-- </div> --> */}
@@ -236,6 +345,7 @@ const Chatbox_textbox_cont:FC<ChatboxTextboxCont>= ({addMessage,})=>{
         }
     }
 
+
     return (
         <div className="textbox-input">
             <textarea placeholder="Entrez votre message ici..."
@@ -243,7 +353,7 @@ const Chatbox_textbox_cont:FC<ChatboxTextboxCont>= ({addMessage,})=>{
             onChange={e => setMessage(e.target.value)}
             onKeyDown={handleEnterKey}
             ></textarea>
-            <button type="button" value="Envoyer"  onClick={() => addNewMessage()}>
+            <button className="chat-send" type="button" value="Envoyer"  onClick={() => addNewMessage()}>
                 <IoSendSharp className="react-send-icon" size={"3vh"} /></button>
         </div>
     )
--- projet/chatbox-webapp/frontend-chatbox/src/component/Radio_choix.tsx
+++ projet/chatbox-webapp/frontend-chatbox/src/component/Radio_choix.tsx
@@ -4,26 +4,31 @@ import "../assets/radio-button/src/style.css"
 interface Component_Radio {
     titles: string[],
     elements_id_array: string[][],
-    texts_array:string[][]
+    texts_array:string[][],
+    addToChoices:(e:React.MouseEvent<HTMLInputElement, MouseEvent>)=>void
 }
 
 interface Element_Radio {
     title:string,
     elements_id:string[],
-    texts:string[]
+    texts:string[],
+    radio_id:number,
+    addToChoices:(e:React.MouseEvent<HTMLInputElement, MouseEvent>)=>void
 }
 
 interface Radio_button {
     element_id: string,
-    text:string
+    text:string,
+    radio_id:number,
+    addToChoices:(e:React.MouseEvent<HTMLInputElement, MouseEvent>)=>void
 }
 // https://freefrontend.com/css-radio-buttons/
 // https://freefrontend.com/assets/zip/css-radio-buttons/2023-radio-button.zip
 
 
-const Radio_element : FC<Element_Radio> = ({title,elements_id,texts}) => {
+const Radio_element : FC<Element_Radio> = ({title,elements_id,texts,radio_id,addToChoices}) => {
     const radio_buttons =  texts.map((txt,index) => {
-        return <Radio_button  key={index} element_id={elements_id[index]} text={txt} />
+        return <Radio_button addToChoices={ addToChoices} radio_id={radio_id} key={index} element_id={elements_id[index]} text={txt} />
     })
 
     return (
@@ -33,14 +38,16 @@ const Radio_element : FC<Element_Radio> = ({title,elements_id,texts}) => {
                 <p>{title}</p>
                 {radio_buttons}
             </div>
+            
         </div>
         </>
     )
 }
 
-const Radio_button : FC<Radio_button> = ({element_id,text}) => {
+const Radio_button : FC<Radio_button> = ({element_id,text,radio_id,addToChoices}) => {
+    const radioName = "radio"+radio_id
     return (
-        <div className="radio-item"><input name="radio" id={element_id} type="radio"/><label htmlFor={element_id}>{text}</label></div>
+        <div className="radio-item"><input onClick={(e)=>addToChoices(e)} name={radioName} value={element_id} id={element_id+radio_id} type="radio"/><label  htmlFor={element_id+radio_id}>{text}</label></div>
     )
 }
 
